4c6868bf0f4a4785da8942b5e6d48e931f3116f8
Erwin Pascual <epas97@hotmail.com>
2023-02-16 16:09:44 -0700

    Add Implementation for Day 6 (LCP 5 Longest Palindromic Substring)

---
+++ DailyLeetCode/DailyLeetCode/Problems/Day6.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+namespace DailyLeetCode.Problems
+{
+    /// <summary>
+    /// Leet Code Problem 5: Longest Palindromic Substring
+    /// 
+    /// Given a string s, return the longest palindromic substring in s.
+    /// </summary>
+    public class Day6 : ITest
+    {
+        string LongestPalindrome(string s)
+        {
+            int length = 0;
+            string palindrome = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                // Odd Case
+                int L = i;
+                int R = i;
+                while (L >= 0 && R < s.Length && s[L] == s[R])
+                {
+                    if (R - L + 1 > length)
+                    {
+                        length = R - L + 1;
+                        palindrome = s.Substring(L, length);
+                    }
+
+                    L -= 1;
+                    R += 1;
+                }
+
+
+                // Even Case
+                L = i;
+                R = i + 1;
+
+                while (L >= 0 && R < s.Length && s[L] == s[R])
+                {
+                    if (R - L + 1 > length)
+                    {
+                        length = R - L + 1;
+                        palindrome = s.Substring(L, length);
+                    }
+
+                    L -= 1;
+                    R += 1;
+                }
+            }
+
+            return palindrome;
+        }
+
+        public void Test()
+        {
+            string case1 = "baba";
+            string case2 = "cbaab";
+            string case3 = "abcdabccba";
+
+            Assert.AreEqual("bab", LongestPalindrome(case1));
+            Assert.AreEqual("baab", LongestPalindrome(case2));
+            Assert.AreEqual("abccba", LongestPalindrome(case3));
+        }
+    }
+}
--- DailyLeetCode/DailyLeetCode/Program.cs
+++ DailyLeetCode/DailyLeetCode/Program.cs
@@ -29,11 +29,13 @@ internal class Program
         Day3 day3 = new Day3();
         Day4 day4 = new Day4();
         Day5 day5 = new Day5();
+        Day6 day6 = new Day6();
 
         _problems.Add(day1);
         _problems.Add(day2);
         _problems.Add(day3);
         _problems.Add(day4);
         _problems.Add(day5);
+        _problems.Add(day6);
     }
 }
